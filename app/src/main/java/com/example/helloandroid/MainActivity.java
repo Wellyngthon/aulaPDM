e8a0f198160ecd22c08c70902637eda2f059f542
wellyngton <wgl_35@hotmail.com>
2020-09-03 18:50:24 -0400

    Navegacao com intent

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.helloandroid;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +39,34 @@ public class MainActivity extends AppCompatActivity {
         //Mostra uma notificacao Toast
         Toast.makeText(getApplicationContext(),"Ola"+campoTexto.getText(),Toast.LENGTH_LONG).show();
 
+    }
+    public void alerta(View view){
+        //cria o builder
+        //AlertDialog.Builder alerta = new AlertDialog.Builder(this, R.style.Theme_AppCompat_NoActionBar); COM ESTILO
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this); // SEM ESTILO
+        //alerta.setMessage("Alerta de teste").create().show();  OPCIONAL
+        alerta.setMessage("Alerta de teste");
+
+        alerta.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"Ola"+campoTexto.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alerta.setNegativeButton("Cancelar",null);
+
+        //cria o dialog
+        AlertDialog dialog = alerta.create();
+        dialog.show(); // chama a msg
+
+
+
+    }
+
+    public void segundaTela(View view){
+        Intent it = new Intent(MainActivity.this, SegundaActivity.class);
+        startActivity(it);
     }
 
 }
